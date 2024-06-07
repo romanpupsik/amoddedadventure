d8c04adaae30a6d2c19d1226481aea9e1629b18f
romanpupsik <120100772+romanpupsik@users.noreply.github.com>
2024-06-07 16:37:19 +0300

    Update AModdedAdventure.lua

--- AModdedAdventure.lua
+++ AModdedAdventure.lua
@@ -7,7 +7,7 @@ if game.PlaceId == 17041031058 then
         Time = 5
     })
 
-    local Window = OrionLib:MakeWindow({Name = "Cursed Hub", HidePremium = true, IntroText = "Cursed Hub", SaveConfig = true, ConfigFolder = "AModdedAdventureFolder"})
+    local Window = OrionLib:MakeWindow({Name = "Cursed Hub", HidePremium = true, SaveConfig = true, ConfigFolder = "AModdedAdventureFolder", IntroEnabled = true, IntroText = "Cursed Hub"})
 
     local AutofarmTab = Window:MakeTab({
         Name = "Autofarm",
